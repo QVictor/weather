e0f18ef0037f41a54dc176dcbb02e51c9b6d56d1
QVictor <qqlogin@yandex.ru>
2024-01-11 09:09:41 +0300

    Add vue component for testing open weather api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import GetWeather from './components/GetWeather.vue'
 
 const app = createApp()
+
+app.component('get_weather', GetWeather)
 
 app.mount('#app')
---
+++ resources/js/components/GetWeather.vue
@@ -0,0 +1,28 @@
+<script>
+import { ref } from 'vue'
+import axios from 'axios';
+
+export default {
+    data: function () {
+        return {
+            companies: [],
+            count: 0
+        }
+    },
+    mounted() {
+        var app = this;
+        axios.get('/api/getWeather')
+            .then(function (resp) {
+                app.companies = resp.data;
+                console.log(resp.data);
+            })
+            .catch(function (resp) {
+                console.log(resp);
+            });
+    }
+}
+</script>
+
+<template>
+    {{ companies }}
+</template>
