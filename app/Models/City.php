571f79031fb0d13586362377c7162ae475c427d5
QVictor <qqlogin@yandex.ru>
2024-01-12 08:13:57 +0300

    Add relations between city and country models

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class City extends Model
 {
     use HasFactory;
 
     protected $table = 'cities';
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'two_letters_code', 'country_code');
+    }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     use HasFactory;
 
     protected $table = 'countries';
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class, 'country_code', 'two_letters_code');
+    }
 }
