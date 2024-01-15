84fc58a9af4eb61aa717aae4eacd7a2d28c1973d
QVictor <qqlogin@yandex.ru>
2024-01-15 09:32:55 +0300

    Save data for weather in all cities in database

--- app/Services/OpenWeatherService.php
+++ app/Services/OpenWeatherService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Helpers\CityHelper;
 use App\Models\OpenWeatherData;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use PHPUnit\Exception;
@@ -38,6 +39,25 @@ class OpenWeatherService
     {
         $jsonString = $answer->getBody()->getContents();
         return json_decode($jsonString, true);
+    }
 
+    private function prepareDataFromInsert($apiAnswer): array
+    {
+        $res = [];
+        foreach ($apiAnswer['list'] as $city) {
+            $res[] = [
+                'city_id' => $city['id'],
+                'weather_condition_code' => $city['weather'][0]['id'],
+                'weather_condition_main_type' => $city['weather'][0]['main'],
+                'weather_condition_description' => $city['weather'][0]['description'],
+                'weather_condition_icon' => $city['weather'][0]['icon'],
+                'temp' => $city['main']['temp'],
+                'humidity' => $city['main']['humidity'],
+                'wind_speed' => $city['wind']['speed'],
+                'created_at' => Carbon::createFromTimestamp($city['dt']),
+                'updated_at' => Carbon::createFromTimestamp($city['dt']),
+            ];
+        }
+        return $res;
     }
 }
