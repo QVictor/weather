010be967698a8a9c4b6cd0072d44ea18453861f5
QVictor <qqlogin@yandex.ru>
2024-01-15 08:44:22 +0300

    Add relations between city and open weather data models

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -15,5 +16,10 @@ class City extends Model
     public function country(): BelongsTo
     {
         return $this->belongsTo(Country::class, 'two_letters_code', 'country_code');
+    }
+
+    public function open_weather_data(): HasMany
+    {
+        return $this->hasMany(OpenWeatherData::class, 'city_id', 'open_weather_city_id');
     }
 }
--- app/Models/OpenWeatherData.php
+++ app/Models/OpenWeatherData.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OpenWeatherData extends Model
 {
     use HasFactory;
 
     protected $table = 'open_weather_data';
+
+    public function city():BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city_id', 'open_weather_city_id');
+    }
 }
